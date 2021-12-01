e0411f67f507f6c7d7f9e8efce7a207025036bd8
Pugalotl <94671281+Pugalotl@users.noreply.github.com>
2021-12-01 20:49:35 +1100

    Added files
    Added the html files

---
+++ coded.js
@@ -0,0 +1,65 @@
+
+
+function generate() {
+	const items = [
+	[0, 0],
+	[0, 1],
+	[0, 2],
+	[0, 3],
+	[0, 4], //end
+	[1, 0],
+	[2, 0],
+	[3, 0],
+	[4, 0],
+	[4, 1], //end
+	[3, 1],
+	[2, 1],
+	[1, 1],
+	[1, 2],
+	[1, 3], //end
+	[3, 2],
+	[2, 2],
+	[2, 3],
+	[2, 4],
+	[1, 4], //end
+	[4, 2],
+	[4, 3],
+	[3, 3],
+	[3, 4],
+	[4, 4],
+	];
+	
+	var canvas = document.getElementById("myCanvas");
+	var ctx = canvas.getContext("2d");
+	var lettercheck = 0
+	for (let points = 0; points < 5; points++)
+	{
+	let stringd = ""
+	let current = document.getElementById("input").value
+	if (current.charAt(lettercheck) == " ")
+	{
+	stringd = "00000"	
+	}
+	else
+	{
+    stringd = dec2bin(current.charAt(lettercheck).charCodeAt() - 96)
+	}
+	while (stringd.length < 5)
+	{
+	stringd = "0" + stringd	
+	}
+	for (let i = 0; i < 5; i++)
+	{
+	if (stringd.charAt(i) == 1)
+	{
+	ctx.fillRect(items[i+(5*lettercheck)][1]*16, items[i+(5*lettercheck)][0]*16, 16, 16);
+	}
+	}
+	lettercheck += 1;
+	}
+}
+
+
+function dec2bin(dec) {
+  return (dec >>> 0).toString(2);
+}
